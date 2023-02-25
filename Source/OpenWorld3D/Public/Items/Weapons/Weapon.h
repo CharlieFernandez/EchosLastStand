ae235a1f767a4f9f02f6496075030b57bbc299ec
CharlieFernandez <charliefernandez777@gmail.com>
2023-02-25 11:30:41 -0500

    Weapons class and blueprint created and added to map.

---
+++ Source/OpenWorld3D/Public/Items/Weapons/Weapon.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENWORLD3D_API AWeapon : public AItem
+{
+	GENERATED_BODY()	
+};
