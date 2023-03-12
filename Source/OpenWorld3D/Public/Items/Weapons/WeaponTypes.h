2d176ac8ee9c32800c7be9d6e1ec43f373f24cfe
CharlieFernandez <charliefernandez777@gmail.com>
2023-03-12 12:38:05 -0400

    Added WeaponTypes enum to keep track of whether or not an item is one-handed or two-handed. Haven't implemented it yet, though.

---
+++ Source/OpenWorld3D/Public/Items/Weapons/WeaponTypes.h
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+UENUM()
+enum class EWeaponType: uint8
+{
+	EWT_OneHanded UMETA(DisplayName = "One-Handed"),
+	EWT_TwoHanded UMETA(DisplayName = "Two-Handed")
+};
