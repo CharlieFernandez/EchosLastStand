9f043a385cfacc6c45b8578bc6051253c4d503dc
CharlieFernandez <charliefernandez777@gmail.com>
2023-04-04 18:14:56 -0400

    Restructured item blueprints a bit.

---
+++ Source/OpenWorld3D/Private/Items/Souls.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Souls.h"
+
+void ASouls::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+}
